e503d4f75e33e390ffbcadc8a01e61eb2d82e2fe
Mariia <maria.gula0307@gmail.com>
2024-09-16 14:50:25 +0300

    make build

---
+++ index.4e23850f.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest(".list-item__link");r&&(t.src=r.href)});
+//# sourceMappingURL=index.4e23850f.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const gallery = document.querySelector(".gallery");
+const largeImg = document.getElementById("largeImg");
+gallery.addEventListener("click", (e)=>{
+    e.preventDefault();
+    const imgLink = e.target.closest(".list-item__link");
+    if (!imgLink) return;
+    largeImg.src = imgLink.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
